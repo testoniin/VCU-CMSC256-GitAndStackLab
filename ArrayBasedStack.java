cdcc3fe061deb105853f97263d9eb45267aecfcb
Isabella N Testoni <testoniin@RAMS.adp.vcu.edu>
2018-02-13 15:32:09 -0500

    Started ArrayBasedStack implementation

---
+++ ArrayBasedStack.java
@@ -0,0 +1,18 @@
+
+public class ArrayBasedStack implements StackInterface{
+   private data;//
+   private int topOfStack;
+   private int INITIAL_CAPACITY = 5;
+   private boolean initialized;
+   
+   public ArrayBasedStack(int INITIAL_CAPACITY){
+      if(!INITIAL_CAPACITY > 0){
+         throw new IllegalArgumentException
+      
+      }
+   }
+    
+
+
+
+}
